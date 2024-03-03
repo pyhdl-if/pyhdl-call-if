d2620bdd3e1c5fad4fb94dbb303efb9382e52edf
Matthew Ballance <matt.ballance@gmail.com>
2024-03-03 07:47:35 -0800

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- tests/smoke/obj_dir/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
- /project/tools/verilator/5.018/share/verilator/include/verilated_threads.cpp \
- /project/tools/verilator/5.018/share/verilator/include/verilatedos.h \
- /project/tools/verilator/5.018/share/verilator/include/verilated_threads.h \
- /project/tools/verilator/5.018/share/verilator/include/verilated.h \
- /project/tools/verilator/5.018/share/verilator/include/verilated_config.h \
- /project/tools/verilator/5.018/share/verilator/include/verilated_types.h \
- /project/tools/verilator/5.018/share/verilator/include/verilated_funcs.h
